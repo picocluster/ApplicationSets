67c17253dbeb1e0027bba838f7c2f4cc1c477aca
Craig Brown <imarcticblue@gmail.com>
2020-02-29 15:29:45 -0700

    Fix resize script

--- rock_pi/cluster_setup/Pico5/resize_rockpi.sh
+++ rock_pi/cluster_setup/Pico5/resize_rockpi.sh
@@ -1,5 +1,5 @@
-sgdisk -e /dev/mmcblk1
-sgdisk -d 5 /dev/mmcblk1
-sgdisk -N 5 /dev/mmcblk1
-partprobe /dev/mmcblk1
-resize2fs /dev/mmcblk1p5
+sgdisk -e /dev/mmcblk0
+sgdisk -d 5 /dev/mmcblk0
+sgdisk -N 5 /dev/mmcblk0
+partprobe /dev/mmcblk0
+resize2fs /dev/mmcblk0p5
