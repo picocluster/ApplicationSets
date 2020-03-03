f6fb50f51db60795062495b6f283155bbde105fa
Craig Brown <imarcticblue@gmail.com>
2020-03-02 21:09:06 -0700

    Update rockpi resize scripts

--- rock_pi/cluster_setup/Pico3/resizeAllNodes.sh
+++ rock_pi/cluster_setup/Pico3/resizeAllNodes.sh
@@ -2,5 +2,5 @@
 for x in 2 1 0
 do
 echo "resizing pc$x"
-ssh pc$x 'sudo ./resize_rockpi.sh'
+ssh pc$x 'sudo /usr/sbin/resize-helper'
 done
--- rock_pi/cluster_setup/Pico5/resize_rockpi.sh
+++
@@ -1,5 +0,0 @@
-sgdisk -e /dev/mmcblk0
-sgdisk -d 5 /dev/mmcblk0
-sgdisk -N 5 /dev/mmcblk0
-partprobe /dev/mmcblk0
-resize2fs /dev/mmcblk0p5
